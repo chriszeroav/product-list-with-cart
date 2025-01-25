7efa00d5356ee0cedcdce6531993eb74b6328614
Christopher Romero <chris.zeroav@gmail.com>
2025-01-25 08:41:38 -0500

    base structure for this project done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +10,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "red-custom": "hsl(14, 86%, 42%)",
+        "green-custom": "hsl(159, 69%, 38%)",
+        "rose-custom": {
+          50: "hsl(20, 50%, 98%)",
+          100: "hsl(13, 31%, 94%)",
+          300: "hsl(14, 25%, 72%)",
+          400: "hsl(7, 20%, 60%)",
+          500: "hsl(12, 20%, 44%)",
+          900: "hsl(14, 65%, 9%)",
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-red-hat-text)"],
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
